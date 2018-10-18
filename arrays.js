6a1d545d1329a045da929ea1e15cae6e9177b7d6
Ryan Daley <rdaley321@yahoo.com>
2018-10-18 08:44:35 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,9 +1,10 @@
 var chocolateBars = ['snickers','hundred grand','kitkat','skittles']
 
 function addElementToBeginningOfArray(array, element) {
-  
+  return [element, ...array]
 }
 
-function destructivelyAddElementToBeginningOfArray() {
-  
+function destructivelyAddElementToBeginningOfArray(array, element) {
+  array.push(element)
+  return array
 }
